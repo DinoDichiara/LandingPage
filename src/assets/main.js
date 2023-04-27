e97d45e0d5a99324b492ca1fee96fb97ad527add
DinoDichiara <dichiaradino951@gmail.com>
2023-04-27 09:51:08 -0300

    listo para lanzar la pagina

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,6 +1,6 @@
-const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCS3eKy88CbT4HG6slJNKpUw&part=snippet%2Cid&order=date&maxResults=9';
-
 const content = null || document.getElementById('content')
+
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCY2ogSxB2beBNBRMKU_dXzA&part=snippet%2Cid&order=date&maxResults=9';
 
 const options = {
 	method: 'GET',
@@ -12,32 +12,34 @@ const options = {
 }
 
 async function fetchData(urlApi) {
-    const response = await fetch(urlApi, options)
-    const data = await response.json()
+    const respnse = await fetch(urlApi, options)
+    const data = await respnse.json()
     return data
 }
 
 (async () => {
-    try{
+    try {
         const videos = await fetchData(API)
         let view = `
-        ${videos.items.map(video => `
-            <div class="group relative">
-                <div
-                    class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                    <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
-                </div>
-                <div class="mt-4 flex justify-between">
-                    <h3 class="text-sm text-gray-700">
-                        <span aria-hidden="true" class="absolute inset-0"></span>
-                        ${video.snippet.title}
-                    </h3>
-                </div>
+        ${videos.items.map((video) => `
+          <div class="group relative">
+            <div
+              class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+              <img src="${video.snippet.thumbnails.high.url}" alt="" class="w-full">
             </div>
-        `).slice(0,4).join('')}
+            <div class="mt-4 flex justify-between">
+              <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">
+              <h3 class="text-sm text-gray-700">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                ${video.snippet.title}
+              </h3>
+              </a>
+            </div>
+          </div>
+        `).slice(0,8).join(' ')}
         `
         content.innerHTML = view
     } catch (error) {
         console.log(error)
     }
-})() 
+})()
